052d00a70d1a30cc8452b61694fdc36f5c6aa23b
Manoj <mkdyanugk@gmail.com>
2020-03-09 21:29:36 +0530

    changes after adding amplify default sign up sign in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
@@ -21,4 +26,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App,true);
